94e1af31bda305065d1c1a1867e59f8b96a7e59c
Greg Hart <BNFRIqC2q144@outlook.com>
2026-01-07 12:16:01 -0600

    Add files via upload

---
+++ winstacleaner.ps1
@@ -0,0 +1,47 @@
+﻿Function Clean-Up ([String]$FileType, [Array]$Data)
+{
+	#Reading list of files from C:\Windows\Installer folder.
+	$InstallerFiles = Get-ChildItem "C:\Windows\Installer" "*.$FileType" -FileType
+	foreach ($InstallerFile in $InstallerFiles)
+	{
+		#If Windows Installer doens't have data about existing file in C:\Windows\Installer folder,
+		#we can assume it not needed and we delete the file.
+		If (!($Data | Where {$_ -eq $InstallerFile.FullName}))
+		{
+			Remove-Item -Path $InstallerFile.FullName -Force -ErrorAction Continue -WhatIf
+		}
+	}
+}
+
+#Initializing arrays
+[array]$PatchesLocation = @()
+[array]$InstallersLocation = @()
+
+#Finding info about installed programs.
+$Installer = New-Object -ComObject "WindowsInstaller.Installer"
+$Products = $Installer.Products()
+
+#Going through products and finding patches related to every products
+foreach ($ProductCode in $Products)
+{
+	#Adding path of the .msi files into the array, which we will process later.
+	$InstallersLocation += $Installers.ProductInfo($ProductCode, "LocalPackage")
+	
+	#Find patches which are installed for certain product code
+	$Patches = $Installer.Patches($ProductCode)
+	foreach ($PatchCode in $Patches)
+	{
+		#Adding path of the .msp files into the array, which we will process later.
+		$PatchesLocation += $Installer.PatchInfo($PatchCode, "LocalPackage")
+	}
+}
+
+#Use the function to compare date with the files in the C:\Windows\Installer directory.
+If ( $PatchesLocation.Count -ne 0)
+{
+	Clean-Up -FileType "msp" -Data $PatchesLocation
+}
+If ( $InstallersLocation.Count -ne 0)
+{
+	Clean-Up -FileType "msi" -Data $InstallersLocation
+}
